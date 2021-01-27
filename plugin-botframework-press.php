5c1e3c8bc7678ebc09b80db843e8a498c1c18b5c
JeanC Semprit <jc.semprit@hotmail.com>
2021-01-27 19:01:20 -0400

    First commmit, add initial files

---
+++ plugin-botframework-press.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Bot Framework Press
+ * 
+ * Plugin Name: Bot Framework Press
+ * Description: A Bot Framework Content Manager plugin.
+ * Version: 0.1.0 
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Text Domain: my-basics-plugin
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ /*
+    Bot Framework Press is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    any later version.
+    
+    Bot Framework Press is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with Bot Framework Press. If not, see https://www.gnu.org/licenses/gpl-2.0.html. 
+*/
+ ?>
