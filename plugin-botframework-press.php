0aa37ec4890ca522fca32f9f444a140cafd23b11
JeanC Semprit <jc.semprit@hotmail.com>
2021-01-29 19:17:23 -0400

    Clean plugin-botframework-press.php and add package.json file

--- plugin-botframework-press.php
+++ plugin-botframework-press.php
@@ -27,4 +27,9 @@
     You should have received a copy of the GNU General Public License
     along with Bot Framework Press. If not, see https://www.gnu.org/licenses/gpl-2.0.html. 
 */
+
+// Playing with the Admin menu
+// Reference: 
+// https://themes.artbees.net/blog/wordpress-custom-admin-pages/
+
 ?>
